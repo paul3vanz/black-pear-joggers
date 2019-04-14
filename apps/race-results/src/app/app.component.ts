4ae46e64142f8b1701c4199070a8d55df1e3ff41
Paul Evans <paul3vanz@gmail.com>
2019-04-14 10:17:42 +0100

    Fix base href

--- apps/race-results/src/app/app.component.ts
+++ apps/race-results/src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'black-pear-joggers-root',
+  selector: 'bpj-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: [ './app.component.scss' ],
 })
 export class AppComponent {
   title = 'race-results';
