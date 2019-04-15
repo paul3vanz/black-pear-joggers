01cb78f6c3d47ba23f9c68738a36fc0b3e578ce8
paul3vanz <paul@3vanz.co.uk>
2019-04-15 12:05:03 +0100

    Update club-standards.service.ts

--- apps/club-standards/src/app/services/club-standards.service.ts
+++ apps/club-standards/src/app/services/club-standards.service.ts
@@ -7,6 +7,7 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root',
 })
 export class ClubStandardsService {
+  
   standardsUrl = '//bpj.org.uk/api/public/index.php/standards';
 
   constructor(private http: HttpClient) {}
