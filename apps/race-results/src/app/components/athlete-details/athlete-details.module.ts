ad159f15c0b2f0013b5161a36b451d7838f535db
Paul Evans <paul3vanz@gmail.com>
2019-05-11 07:54:10 +0100

    Fix lint issues

--- apps/race-results/src/app/components/athlete-details/athlete-details.module.ts
+++ apps/race-results/src/app/components/athlete-details/athlete-details.module.ts
@@ -1,18 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AthleteDetailsComponent } from './athlete-details.component';
-import { PipesModule } from './../pipes/pipes.module';
+import { PipesModule } from '../../pipes/pipes.module';
 
 @NgModule({
-    imports: [
-        CommonModule,
-        PipesModule,
-    ],
-    declarations: [
-        AthleteDetailsComponent,
-    ],
-    exports: [
-        AthleteDetailsComponent,
-    ],
-  })
-  export class AthleteDetailsModule {}
+  imports: [ CommonModule, PipesModule ],
+  declarations: [ AthleteDetailsComponent ],
+  exports: [ AthleteDetailsComponent ],
+})
+export class AthleteDetailsModule {}
