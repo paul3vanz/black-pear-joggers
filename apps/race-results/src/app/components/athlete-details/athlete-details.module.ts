89108793c236b02cf3f635384a45fde00c063c7c
paul3vanz <paul@3vanz.co.uk>
2019-05-11 06:41:47 +0100

    Removed import

--- apps/race-results/src/app/components/athlete-details/athlete-details.module.ts
+++ apps/race-results/src/app/components/athlete-details/athlete-details.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AthleteDetailsComponent } from './athlete-details.component';
-import { PipesModule } from '../../pipes/pipes.module';
-import { PersonalBestModule } from '../personal-best-panel/personal-best-panel.module';
+import { PipesModule } from './../pipes/pipes.module';
 
 @NgModule({
     imports: [
         CommonModule,
-        PersonalBestModule,
         PipesModule,
     ],
     declarations: [
