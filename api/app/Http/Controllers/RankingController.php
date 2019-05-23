bb8c1cb04b7dae28d2365adaec27f87fabf787d7
Paul Evans <paul3vanz@gmail.com>
2019-05-23 09:29:00 +0100

    Add ranking endpoint

---
+++ api/app/Http/Controllers/RankingController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Ranking;
+use Illuminate\Support\Facades\Artisan;
+
+class RankingController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct() {
+
+    }
+
+    public function getRankingsByAthlete($athleteId, $year = null) {
+        $rankings = Ranking::where('athlete_id', $athleteId)
+            ->when($year, function($query, $year) {
+              return $query->whereYear('date', $year);
+            })
+            ->get()
+            ->all();
+
+        return response()->json($rankings);
+    }
+}
--- api/routes/web.php
+++ api/routes/web.php
@@ -41,6 +41,9 @@ $router->get('/parkruntourists', 'PerformanceController@getParkrunTourists');
 $router->get('/performances', 'PerformanceController@getPerformanceSummaries');
 $router->get('/performances/{date}/{meeting}', 'PerformanceController@getPerformancesByMeeting');
 
+$router->get('/rankings/{athleteId}', 'RankingController@getRankingsByAthlete');
+$router->get('/rankings/{athleteId}/{year}', 'RankingController@getRankingsByAthlete');
+
 $router->get('/records', 'PerformanceController@getRecords');
 $router->post('/records/query', 'PerformanceController@queryRecord');
 
