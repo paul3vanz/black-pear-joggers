469c1d809026992891203a6fff0836c62a5aaafb
Paul Evans <paul3vanz@gmail.com>
2020-10-09 23:47:45 +0100

    Magic mile form usability

--- apps/admin/src/app/magic-mile/components/record-magic-mile-form/record-magic-mile-form.component.ts
+++ apps/admin/src/app/magic-mile/components/record-magic-mile-form/record-magic-mile-form.component.ts
@@ -1,22 +1,22 @@
-import { Component, Output, EventEmitter } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
-import { Actions, ofType } from '@ngrx/effects';
-
 import * as moment from 'moment-mini-ts';
 
-import { MagicMileLocation } from 'libs/magic-mile-data-access/src/lib/models/magic-mile-location.model';
-import { Athlete } from 'apps/race-results/src/app/models/athlete';
+import { Actions, ofType } from '@ngrx/effects';
+import { Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 import { Observable, forkJoin } from 'rxjs';
-import { Store } from '@ngrx/store';
-import { distinctUntilChanged, debounceTime, filter } from 'rxjs/operators';
+import { createResultSuccess, magicMileActions, magicMileQuery } from '@black-pear-joggers/magic-mile-data-access';
+import { debounceTime, distinctUntilChanged, filter } from 'rxjs/operators';
+
+import { Athlete } from 'apps/race-results/src/app/models/athlete';
 import { LoadingState } from '@black-pear-joggers/authentication';
-import { magicMileQuery, createResultSuccess, magicMileActions } from '@black-pear-joggers/magic-mile-data-access';
+import { MagicMileLocation } from 'libs/magic-mile-data-access/src/lib/models/magic-mile-location.model';
+import { Store } from '@ngrx/store';
 import { ToastService } from 'libs/shared-components/src/lib/services/toast.service';
 
 @Component({
   selector: 'bpj-record-magic-mile-form',
   templateUrl: './record-magic-mile-form.component.html',
-  styleUrls: ['./record-magic-mile-form.component.scss']
+  styleUrls: ['./record-magic-mile-form.component.scss'],
 })
 export class RecordMagicMileFormComponent {
   athletes$: Observable<Athlete[]>;
@@ -25,9 +25,11 @@ export class RecordMagicMileFormComponent {
 
   @Output() create = new EventEmitter<any>();
 
+  @ViewChild('firstName') firstName: ElementRef<HTMLInputElement>;
+
   initialFormState = {
-    date: [ moment().format('YYYY-MM-DD'), Validators.required ],
-    location: [ this.getDefaultLocation(), Validators.required ],
+    date: [moment().format('YYYY-MM-DD'), Validators.required],
+    location: [this.getDefaultLocation(), Validators.required],
     athleteId: [],
     firstName: ['', Validators.required],
     lastName: ['', Validators.required],
@@ -39,45 +41,47 @@ export class RecordMagicMileFormComponent {
     actualTimeParsed: ['', Validators.required],
   };
 
+  keysResetOnSave = ['athleteId', 'firstName', 'lastName', 'gender', 'category'];
+
   form = this.formBuilder.group(this.initialFormState);
 
-  constructor(
-    private formBuilder: FormBuilder,
-    private store$: Store<any>,
-    private actions$: Actions,
-    private toastService: ToastService
-  ) {
+  constructor(private formBuilder: FormBuilder, private store$: Store<any>, private actions$: Actions, private toastService: ToastService) {
     this.callState$ = this.store$.select(magicMileQuery.getCallState);
     this.athletes$ = this.store$.select(magicMileQuery.getAthletes);
 
     this.actions$.pipe(ofType(createResultSuccess)).subscribe(() => {
-        this.toastService.pushMessage('Record created');
+      this.toastService.pushMessage('Record created');
 
-      const formDefault = {};
-
-      Object.keys(this.initialFormState).forEach((key) => {
-        formDefault[key] = this.initialFormState[key][0];
+      this.keysResetOnSave.forEach((key) => {
+        this.form.controls[key].reset(this.initialFormState[key][0]);
       });
 
-      this.form.reset(formDefault);
+      this.firstName.nativeElement.focus();
+
       this.showErrors = false;
     });
 
-    this.form.get('firstName').valueChanges.pipe(
-      debounceTime(500),
-      distinctUntilChanged(),
-      filter((value) => value && value.length >= 2),
-    ).subscribe(() => {
-      this.emitAthleteName();
-    });
+    this.form
+      .get('firstName')
+      .valueChanges.pipe(
+        debounceTime(500),
+        distinctUntilChanged(),
+        filter((value) => value && value.length >= 2)
+      )
+      .subscribe(() => {
+        this.emitAthleteName();
+      });
 
-    this.form.get('lastName').valueChanges.pipe(
-      debounceTime(500),
-      distinctUntilChanged(),
-      filter((value) => value && value.length >= 2),
-    ).subscribe(() => {
-      this.emitAthleteName();
-    });
+    this.form
+      .get('lastName')
+      .valueChanges.pipe(
+        debounceTime(500),
+        distinctUntilChanged(),
+        filter((value) => value && value.length >= 2)
+      )
+      .subscribe(() => {
+        this.emitAthleteName();
+      });
   }
 
   onSubmit() {
@@ -88,15 +92,26 @@ export class RecordMagicMileFormComponent {
     }
   }
 
-  onChangeTime() {
+  onChangePredictedTime() {
+    this.form.get('actualTime').setValue(this.form.get('predictedTime').value);
+    this.updateParsedTimeValues();
+  }
+
+  onChangeActualTime() {
+    this.updateParsedTimeValues();
+  }
+
+  updateParsedTimeValues() {
     this.form.get('predictedTimeParsed').setValue(this.convertTimeToSeconds(this.form.get('predictedTime').value));
     this.form.get('actualTimeParsed').setValue(this.convertTimeToSeconds(this.form.get('actualTime').value));
   }
 
   emitAthleteName() {
-    this.store$.dispatch(magicMileActions.searchAthletes({
-      name: `${this.form.get('firstName').value} ${this.form.get('lastName').value}`.trim()
-    }));
+    this.store$.dispatch(
+      magicMileActions.searchAthletes({
+        name: `${this.form.get('firstName').value} ${this.form.get('lastName').value}`.trim(),
+      })
+    );
   }
 
   onSelectAthlete(athlete: Athlete) {
@@ -110,9 +125,7 @@ export class RecordMagicMileFormComponent {
   }
 
   convertTimeToSeconds(timeString: string) {
-    return timeString
-      ? moment.duration(`00:${timeString}`).asSeconds()
-      : null;
+    return timeString ? moment.duration(`00:${timeString}`).asSeconds() : null;
   }
 
   getDefaultLocation() {
