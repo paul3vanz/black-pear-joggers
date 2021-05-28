eeb8e24b2ea5b9d04bf2a75d9b546db1647bad58
Paul Evans <paul3vanz@gmail.com>
2021-05-28 23:06:44 +0100

    Add standards to magic mile

--- libs/shared-pipes/src/lib/shared-pipes.module.ts
+++ libs/shared-pipes/src/lib/shared-pipes.module.ts
@@ -1,33 +1,18 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { GenderPipe } from './pipes/gender.pipe';
 import { CategoryPipe } from './pipes/category.pipe';
-import { LimitPipe } from './pipes/limit.pipe';
-import { FinishTimePipe } from './pipes/finish-time.pipe';
-import { PacePipe } from './pipes/pace.pipe';
+import { CommonModule } from '@angular/common';
 import { DatePipe } from './pipes/date.pipe';
+import { FinishTimePipe } from './pipes/finish-time.pipe';
+import { FormatAwardPipe } from './pipes/format-award.pipe';
+import { GenderPipe } from './pipes/gender.pipe';
+import { LimitPipe } from './pipes/limit.pipe';
+import { NgModule } from '@angular/core';
+import { PacePipe } from './pipes/pace.pipe';
 import { RaceNamePipe } from './pipes/race-name.pipe';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [
-    GenderPipe,
-    CategoryPipe,
-    LimitPipe,
-    FinishTimePipe,
-    PacePipe,
-    DatePipe,
-    RaceNamePipe
-  ],
+  declarations: [GenderPipe, FormatAwardPipe, CategoryPipe, LimitPipe, FinishTimePipe, PacePipe, DatePipe, RaceNamePipe],
   providers: [PacePipe],
-  exports: [
-    GenderPipe,
-    CategoryPipe,
-    LimitPipe,
-    FinishTimePipe,
-    PacePipe,
-    DatePipe,
-    RaceNamePipe
-  ]
+  exports: [GenderPipe, FormatAwardPipe, CategoryPipe, LimitPipe, FinishTimePipe, PacePipe, DatePipe, RaceNamePipe],
 })
 export class SharedPipesModule {}
