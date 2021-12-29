81c1af661e47bad8721071ff06d9baad9b17d9f4
Paul Evans <paul3vanz@gmail.com>
2021-12-29 22:47:29 +0000

    Error handling

--- next/post-build.js
+++ next/post-build.js
@@ -5,21 +5,23 @@ console.log('Moving static sites');
 const appsFolder = 'dist/apps';
 const apps = fs.readdirSync(appsFolder);
 
-apps.forEach((currentApp) => {
-  const app = fs.readdirSync(appsFolder + '/' + currentApp);
+try {
+  apps.forEach((currentApp) => {
+    const app = fs.readdirSync(appsFolder + '/' + currentApp);
 
-  fs.removeSync(appsFolder + '/' + currentApp + '/.next');
-  fs.removeSync(appsFolder + '/' + currentApp + '/public');
+    fs.removeSync(appsFolder + '/' + currentApp + '/.next');
+    fs.removeSync(appsFolder + '/' + currentApp + '/public');
 
-  try {
-    fs.unlinkSync(appsFolder + '/' + currentApp + '/next.config.js');
-    fs.unlinkSync(appsFolder + '/' + currentApp + '/package.json');
-  } catch {}
+    try {
+      fs.unlinkSync(appsFolder + '/' + currentApp + '/next.config.js');
+      fs.unlinkSync(appsFolder + '/' + currentApp + '/package.json');
+    } catch {}
 
-  fs.copySync(
-    appsFolder + '/' + currentApp + '/exported',
-    appsFolder + '/' + currentApp
-  );
+    fs.copySync(
+      appsFolder + '/' + currentApp + '/exported',
+      appsFolder + '/' + currentApp
+    );
 
-  fs.removeSync(appsFolder + '/' + currentApp + '/exported');
-});
+    fs.removeSync(appsFolder + '/' + currentApp + '/exported');
+  });
+} catch {}
