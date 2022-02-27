823dd85868035fa7a1a701c7a478902b400db9c1
Paul Evans <paul3vanz@gmail.com>
2022-02-27 23:06:25 +0000

    try and fix post build task

--- next/post-build.js
+++ next/post-build.js
@@ -2,46 +2,20 @@ const fs = require('fs-extra');
 
 console.log('Moving static sites');
 
-console.log(getFiles('dist'));
-
 const appsFolder = 'dist/apps';
+const destinationFolder = 'dist/exported';
 const apps = fs.readdirSync(appsFolder);
 
-function getFiles(dir, files_) {
-  files_ = files_ || [];
-  var files = fs.readdirSync(dir);
-  for (var i in files) {
-    var name = dir + '/' + files[i];
-    if (fs.statSync(name).isDirectory()) {
-      getFiles(name, files_);
-    } else {
-      files_.push(name);
-    }
-  }
-  return files_;
-}
+console.log('Clearing ' + destinationFolder);
+fs.removeSync(destinationFolder);
 
-try {
-  apps.forEach((currentApp) => {
-    console.log('Processing ' + currentApp);
+apps.forEach((currentApp) => {
+  console.log('Processing ' + currentApp);
 
-    const app = fs.readdirSync(appsFolder + '/' + currentApp);
+  const app = fs.readdirSync(appsFolder + '/' + currentApp);
 
-    fs.removeSync(appsFolder + '/' + currentApp + '/.next');
-    fs.removeSync(appsFolder + '/' + currentApp + '/public');
-
-    try {
-      fs.unlinkSync(appsFolder + '/' + currentApp + '/next.config.js');
-      fs.unlinkSync(appsFolder + '/' + currentApp + '/package.json');
-    } catch {}
-
-    fs.copySync(
-      appsFolder + '/' + currentApp + '/exported',
-      appsFolder + '/' + currentApp
-    );
-
-    fs.removeSync(appsFolder + '/' + currentApp + '/exported');
-  });
-} catch {}
-
-console.log(getFiles('dist'));
+  const appSourceFolder = appsFolder + '/' + currentApp + '/exported';
+  const appDestinationFolder = destinationFolder + '/' + currentApp;
+  console.log('Copying ' + appSourceFolder + ' to ' + appDestinationFolder);
+  fs.copySync(appSourceFolder, appDestinationFolder);
+});
