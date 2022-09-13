7f508e9d47a6aa26d46d6f63f8d6a48404476502
Paul Evans <paul3vanz@gmail.com>
2022-09-13 18:40:05 +0100

    temp fix eslint error

--- next/apps/vacancies/pages/[vacancy].tsx
+++ next/apps/vacancies/pages/[vacancy].tsx
@@ -17,9 +17,11 @@ type VacancyProps = {
 
 const portableTextComponents: PortableTextComponents = {
   list: {
+    // eslint-disable-next-line react/display-name
     bullet: ({ children }) => <ul className="list-disc pl-5">{children}</ul>,
   },
   listItem: {
+    // eslint-disable-next-line react/display-name
     bullet: ({ children }) => <li className="mb-2">{children}</li>,
   },
 };
