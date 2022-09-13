20a1087c27fb48d2aed1cefbf8681d545a50d38e
Paul Evans <paul3vanz@gmail.com>
2022-09-13 23:23:41 +0100

    add sendgrid to contact form

--- next/apps/contact/pages/index.tsx
+++ next/apps/contact/pages/index.tsx
@@ -7,34 +7,64 @@ import { useState } from 'react';
 
 export function ContactPage() {
   const [isLoading, setIsLoading] = useState(false);
+  const [isSuccess, setIsSuccess] = useState(false);
+  const [isError, setIsError] = useState(false);
 
   function handleSubmit() {
     setIsLoading(true);
+    setIsError(false);
+    setIsSuccess(false);
 
     const myForm = document.getElementById('contact') as HTMLFormElement;
-    const formData = new FormData(myForm);
-    fetch(location.href, {
+    fetch('https://contact.bpj.workers.dev/', {
       method: 'POST',
-      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      body: new URLSearchParams(formData as any).toString(),
-    })
-      .then(() => {
-        // Bodged until I hook up formik or similar
-        (document.getElementById('input-name') as HTMLInputElement).value = '';
-        (document.getElementById('input-email') as HTMLInputElement).value = '';
-        (
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        name: (document.getElementById('input-name') as HTMLInputElement).value,
+        email: (document.getElementById('input-email') as HTMLInputElement)
+          .value,
+        reason: (document.getElementById('input-reason') as HTMLInputElement)
+          .value,
+        message: (
           document.getElementById('input-message') as HTMLTextAreaElement
-        ).value = '';
+        ).value,
+      }),
+    })
+      .then((response) => response.json())
+      .then((response) => {
+        if (response.ok) {
+          setIsSuccess(true);
 
-        alert("Your message was sent successfully. We'll be in touch soon.");
+          // Bodged until I hook up formik or similar
+          (document.getElementById('input-name') as HTMLInputElement).value =
+            '';
+          (document.getElementById('input-email') as HTMLInputElement).value =
+            '';
+          (
+            document.getElementById('input-message') as HTMLTextAreaElement
+          ).value = '';
+        } else {
+          setIsError(true);
+        }
       })
-      .catch((error) => alert(error))
+      .catch((error) => {
+        setIsError(true);
+      })
       .finally(() => setIsLoading(false));
   }
 
   return (
     <>
+      <div>
+        <img
+          src="https://bpj.org.uk/wp-content/uploads/2022/09/club-photo.jpg"
+          alt=""
+          className="w-full object-cover h-64 sm:h-auto"
+          width="1080"
+          height="212"
+        />
+      </div>
+
       <Stack>
         <Container>
           <h1>Contact the club</h1>
@@ -50,6 +80,18 @@ export function ContactPage() {
       <Stack backgroundColour="dark">
         <Container>
           <form className="w-full max-w-lg" data-netlify="true" id="contact">
+            {isSuccess ? (
+              <p className="bg-green-300 px-6 py-4 text-black">
+                Your message was sent successfully.
+              </p>
+            ) : null}
+            {isError ? (
+              <p className="bg-red-300 px-6 py-4 text-black">
+                There was a problem sending your message. Please try again
+                later.
+              </p>
+            ) : null}
+
             <input type="hidden" name="form-name" value="contact" />
 
             <div className="flex flex-wrap -mx-3 mb-6">
