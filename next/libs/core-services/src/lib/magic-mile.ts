81686945c6287c51551b3203fcd1792d8c380426
Paul Evans <paul.evans@purplebricks.com>
2022-09-21 18:36:09 +0100

    switch to authenticated magic mile endpoint

--- next/libs/core-services/src/lib/magic-mile.ts
+++ next/libs/core-services/src/lib/magic-mile.ts
@@ -1,35 +1,33 @@
 import useSWRImmutable from 'swr';
 import { config } from '../config';
-import { fetcher, remove } from './fetcher';
+import { fetcher, post, remove } from './fetcher';
 import { MagicMileResult } from './magic-mile.interface';
-
 
 export const magicMileResultsUrl = `${config.baseApiUrl}/magicmile`;
 
 export function useMagicMileResults() {
-    const { data, error } = useSWRImmutable<MagicMileResult[], string>(magicMileResultsUrl, fetcher);
+  const { data, error } = useSWRImmutable<MagicMileResult[], string>(
+    magicMileResultsUrl,
+    fetcher
+  );
 
-    return {
-        magicMileResults: data,
-        isLoading: !error && !data,
-        isError: error
-    }
-};
+  return {
+    magicMileResults: data,
+    isLoading: !error && !data,
+    isError: error,
+  };
+}
 
-export function createMagicMileResult(magicMileResult: MagicMileResult): Promise<Response> {
-    return fetch(`${config.baseApiUrl}/magicmile`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-          },
-        body: JSON.stringify(magicMileResult),
-    })
+export function createMagicMileResult(
+  magicMileResult: MagicMileResult
+): Promise<Response> {
+  return post(`${config.baseApiUrl}/magicmile`, magicMileResult);
 }
 
 export async function destroy(awardClaim: MagicMileResult): Promise<boolean> {
-    const response = await remove(
-      `${config.baseApiUrl}/magicmile/${awardClaim.id}`
-    );
+  const response = await remove(
+    `${config.baseApiUrl}/magicmile/${awardClaim.id}`
+  );
 
-    return response.ok ? true : false;
-  }
+  return response.ok ? true : false;
+}
