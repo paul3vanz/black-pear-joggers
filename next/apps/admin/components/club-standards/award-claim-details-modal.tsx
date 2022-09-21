d2261335329898078db889cab217f7cb85fbc930
Paul Evans <paul3vanz@gmail.com>
2022-09-21 23:20:50 +0100

    modal styling

--- next/apps/admin/components/club-standards/award-claim-details-modal.tsx
+++ next/apps/admin/components/club-standards/award-claim-details-modal.tsx
@@ -1,4 +1,6 @@
 import { createPortal } from 'react-dom';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export function AwardClaimDetailsModal(props: {
   isOpen: boolean;
@@ -21,6 +23,7 @@ export function AwardClaimDetailsModal(props: {
             <div
               className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"
               aria-hidden="true"
+              onClick={() => props.onClose()}
             ></div>
             <span
               className="hidden sm:inline-block sm:align-middle sm:h-screen"
@@ -30,18 +33,16 @@ export function AwardClaimDetailsModal(props: {
             </span>
             <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
               <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                <FontAwesomeIcon
+                  icon={faTimes}
+                  onClick={() => props.onClose()}
+                  title="Close"
+                  size="2x"
+                  className="absolute right-0 top-0 mt-2 mr-3"
+                />
                 <div className="mt-3 text-center sm:mt-0 sm:text-left">
                   {props.children}
                 </div>
-              </div>
-              <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
-                <button
-                  onClick={() => props.onClose()}
-                  type="button"
-                  className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
-                >
-                  Close
-                </button>
               </div>
             </div>
           </div>
