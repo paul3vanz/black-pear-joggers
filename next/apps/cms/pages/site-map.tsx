c5950d73da2d580953640ce19191dabfcefcc755
Paul Evans <paul3vanz@gmail.com>
2022-11-09 22:38:43 +0000

    fix site map order

--- next/apps/cms/pages/site-map.tsx
+++ next/apps/cms/pages/site-map.tsx
@@ -18,15 +18,17 @@ export default function Page(props: Props) {
         <h2>Pages</h2>
 
         <ul className="list-disc mb-4 ml-5">
-          {props.pages.map((page) => (
-            <li key={page._id}>
-              <Link
-                href={page.slug.current === 'home' ? '/' : page.slug.current}
-              >
-                <a>{page.page.title}</a>
-              </Link>
-            </li>
-          ))}
+          {props.pages
+            .sort((a, b) => (a.slug.current < b.slug.current ? -1 : 0))
+            .map((page) => (
+              <li key={page._id}>
+                <Link
+                  href={page.slug.current === 'home' ? '/' : page.slug.current}
+                >
+                  <a>{page.page.title}</a>
+                </Link>
+              </li>
+            ))}
         </ul>
 
         <h2>News</h2>
