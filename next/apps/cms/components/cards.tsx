b889fdc410dc6b3a796f4053c17bd110ea7ee15e
Paul Evans <paul3vanz@gmail.com>
2022-11-11 23:21:46 +0000

    try and stop clash being tree shaken out

--- next/apps/cms/components/cards.tsx
+++ next/apps/cms/components/cards.tsx
@@ -5,7 +5,8 @@ export const Cards = (props: PropsWithChildren<{ maxColumns?: number }>) => (
   <div
     className={classNames(
       'grid md:grid-cols-2 gap-4',
-      props.maxColumns && `xl:grid-cols-${props.maxColumns}`
+      props.maxColumns === 3 && `xl:grid-cols-3`,
+      props.maxColumns === 4 && `xl:grid-cols-4`
     )}
   >
     {props.children}
