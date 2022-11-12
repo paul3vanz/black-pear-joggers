57468e4d36d01783076ced90ea7f6f279c0230a7
Paul Evans <paul3vanz@gmail.com>
2022-11-12 00:08:14 +0000

    add favicon

--- next/apps/cms/pages/_document.tsx
+++ next/apps/cms/pages/_document.tsx
@@ -45,6 +45,37 @@ export default class CustomDocument extends Document<{
             src="https://kit.fontawesome.com/97736414dd.js"
             crossOrigin="anonymous"
           ></script>
+
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/static/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/static/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/static/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/static/site.webmanifest" />
+          <link
+            rel="mask-icon"
+            href="/static/safari-pinned-tab.svg"
+            color="#f89829"
+          />
+          <link rel="shortcut icon" href="/static/favicon.ico" />
+          <meta name="msapplication-TileColor" content="#f89829" />
+          <meta
+            name="msapplication-config"
+            content="/static/browserconfig.xml"
+          />
+          <meta name="theme-color" content="#f89829" />
         </Head>
         <body>
           <Main />
