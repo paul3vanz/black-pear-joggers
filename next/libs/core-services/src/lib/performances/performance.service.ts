df950080d1c6c725517d4220dbf889c731acd428
Paul Evans <paul3vanz@gmail.com>
2022-11-30 00:16:52 +0000

    add date limit to pbs

--- next/libs/core-services/src/lib/performances/performance.service.ts
+++ next/libs/core-services/src/lib/performances/performance.service.ts
@@ -1,3 +1,5 @@
+import { format, subMonths } from 'date-fns';
+
 export function getPersonalBests(): Promise<Response> {
-    return fetch('https://bpj.org.uk/api/public/index.php/performancesindividual?isPersonalBest=1');
+    return fetch(`https://bpj.org.uk/api/public/index.php/performancesindividual?isPersonalBest=1&fromDate=${format(subMonths(new Date(), 1), 'yyyy-MM-dd')}`);
 }
