972079b6578c16c4e708f3c5be87b1cf36c887e5
Paul Evans <paul3vanz@gmail.com>
2022-12-13 22:58:46 +0000

    add profile routing

--- cloudflare/cms/src/config.ts
+++ cloudflare/cms/src/config.ts
@@ -4,21 +4,22 @@ export const netlifyCms = 'https://black-pear-joggers-cms.netlify.app';
 export const netlifyAngularApps = 'https://apps.bpj.org.uk';
 
 export const mappings = [
-  { request: 'club-standards', replace: netlifyAngularApps },
-  { request: 'race-results', replace: netlifyAngularApps },
-  { request: 'magic-mile', replace: netlifyAngularApps },
-  { request: 'club-records', replace: netlifyAngularApps },
+    { request: 'club-standards', replace: netlifyAngularApps },
+    { request: 'race-results', replace: netlifyAngularApps },
+    { request: 'magic-mile', replace: netlifyAngularApps },
+    { request: 'club-records', replace: netlifyAngularApps },
 
-  { request: 'admin', replace: `${cloudflarePages}/admin` },
-  { request: 'contact-the-club', replace: `${cloudflarePages}/contact` },
-  { request: 'contact', replace: `${cloudflarePages}/contact` },
-  { request: 'kit', replace: `${cloudflarePages}/kit` },
-  { request: 'vacancies', replace: `${cloudflarePages}/vacancies` },
-  { request: 'claim-award', replace: `${cloudflarePages}/claim-award` },
-  { request: 'register', replace: `${cloudflarePages}/register` },
+    { request: 'admin', replace: `${cloudflarePages}/admin` },
+    { request: 'contact-the-club', replace: `${cloudflarePages}/contact` },
+    { request: 'contact', replace: `${cloudflarePages}/contact` },
+    { request: 'kit', replace: `${cloudflarePages}/kit` },
+    { request: 'vacancies', replace: `${cloudflarePages}/vacancies` },
+    { request: 'claim-award', replace: `${cloudflarePages}/claim-award` },
+    { request: 'register', replace: `${cloudflarePages}/register` },
+    { request: 'profile', replace: `${cloudflarePages}/profile` },
 
-  { request: 'our-races/croome-race', replace: `${netlifyCms}/our-races/croome-capability-canter` },
-  { request: 'our-races/wild-race', replace: `${netlifyCms}/our-races/the-wild-one` },
+    { request: 'our-races/croome-race', replace: `${netlifyCms}/our-races/croome-capability-canter` },
+    { request: 'our-races/wild-race', replace: `${netlifyCms}/our-races/the-wild-one` },
 ];
 
 export const blockedRegions = ['CN', 'KP', 'SY', 'PK', 'CU'];
