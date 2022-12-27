1209e206e96187b202155616e28cf37008db3773
Paul Evans <paul3vanz@gmail.com>
2022-12-27 11:19:25 +0000

    fix

--- api/app/Http/Controllers/FetchPerformancesController.php
+++ api/app/Http/Controllers/FetchPerformancesController.php
@@ -123,7 +123,7 @@ class FetchPerformancesController extends Controller
             'category' => $performance['category'],
             'date' => $performance['date'],
             'event' => $performance['event'],
-            'meetingId' => $meeting['id'],
+            'meetingId' => $meeting->id,
             'meeting_id' => $performance['meeting_id'],
             'race' => $performance['race'],
             'time_parsed' => $performance['time_parsed'],
