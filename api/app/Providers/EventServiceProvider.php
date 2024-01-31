5e94ad5d814d644b6e1c647eab330df8a562ef99
Paul Evans <paul3vanz@gmail.com>
2024-01-31 07:52:15 +0000

    Update EventServiceProvider.php

--- api/app/Providers/EventServiceProvider.php
+++ api/app/Providers/EventServiceProvider.php
@@ -13,7 +13,7 @@ class EventServiceProvider extends ServiceProvider
      */
     protected $listen = [
         'App\Events\PaymentUpdateReceived' => [
-            'App\Listeners\PaymentUpdateReceived',
+            'App\Listeners\PaymentUpdateReceivedHandler',
         ],
     ];
 }
