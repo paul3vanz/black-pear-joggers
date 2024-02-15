7c49eb5a45182031fd1fcaef2b80ef8533e4e18f
Paul Evans <paul3vanz@gmail.com>
2024-02-15 06:31:26 +0000

    g

--- next/libs/core-services/src/config.ts
+++ next/libs/core-services/src/config.ts
@@ -2,15 +2,20 @@ import { Auth0ProviderOptions } from '@auth0/auth0-react';
 
 interface Config {
     baseApiUrl: string;
-    auth: Auth0ProviderOptions;
+    auth: (redirectUri: string) => Auth0ProviderOptions;
 }
 
 export const config: Config = {
     baseApiUrl: 'https://bpj.org.uk/api/public/index.php',
-    auth: {
+    // baseApiUrl: 'http://localhost:8000',
+    auth: (redirectUri: string) => ({
         domain: 'blackpearjoggers.us.auth0.com',
         clientId: '30P0GEyOCCjXjTI7VtJeAhYwovaJSKq1',
-        audience: 'https://bpj.org.uk',
+        authorizationParams: {
+            audience: 'https://bpj.org.uk',
+            redirect_uri: redirectUri,
+        },
         useRefreshTokens: true,
-    },
+        useRefreshTokensFallback: true,
+    }),
 }
