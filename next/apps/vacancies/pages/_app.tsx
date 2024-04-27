cfeb3ad2321837f4de0965e80d5c20a6a701414b
Paul Evans <paul3vanz@gmail.com>
2024-04-28 00:34:44 +0100

    fix vacancies build

--- next/apps/vacancies/pages/_app.tsx
+++ next/apps/vacancies/pages/_app.tsx
@@ -1,7 +1,6 @@
 import TagManager from 'react-gtm-module';
 import { AppProps } from 'next/app';
-import { Auth0Provider, useAuth0 } from '@auth0/auth0-react';
-import { config } from '@black-pear-joggers/core-services';
+import { Auth0Provider } from '@auth0/auth0-react';
 import { Container } from '@black-pear-joggers/container';
 import { Footer } from '@black-pear-joggers/footer';
 import { Header } from '@black-pear-joggers/header';
@@ -9,6 +8,7 @@ import { PropsWithChildren, useEffect } from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Stack } from '@black-pear-joggers/stack';
 import './styles.css';
+import { config, tagManagerArgs } from '../helpers/config';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -17,10 +17,6 @@ const queryClient = new QueryClient({
     },
   },
 });
-
-const tagManagerArgs = {
-  gtmId: 'GTM-W8KBB3D',
-};
 
 let getAccessTokenSilently = null;
 
@@ -40,23 +36,6 @@ function LoadingContent() {
 }
 
 function PageContent(props: PropsWithChildren<Record<string, unknown>>) {
-  //   const { isLoading, getAccessTokenSilently } = useAuth0();
-
-  //   useEffect(() => {
-  //     (async () => {
-  //       try {
-  //         localStorage.setItem(
-  //           'bpj.token',
-  //           await getAccessTokenSilently({
-  //             audience: config.auth.audience,
-  //           })
-  //         );
-  //       } catch (e) {
-  //         // console.error(e);
-  //       }
-  //     })();
-  //   }, [getAccessTokenSilently]);
-
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
@@ -74,12 +53,7 @@ function CustomApp({ Component, pageProps }: AppProps) {
   }, []);
 
   return (
-    <Auth0Provider
-      {...config.auth}
-      redirectUri={
-        typeof window !== 'undefined' && `${window.location.origin}/admin`
-      }
-    >
+    <Auth0Provider {...config.auth}>
       <QueryClientProvider client={queryClient}>
         <div id="modalContainer"></div>
         <PageContent>
