134a38efdb9eb6d5f7124cf03a857ca479912736
Paul Evans <paul3vanz@gmail.com>
2024-04-28 00:10:53 +0100

    tidy config

--- next/apps/admin/pages/_app.tsx
+++ next/apps/admin/pages/_app.tsx
@@ -2,18 +2,17 @@ import AdminBar from '../components/admin-bar/admin-bar';
 import TagManager from 'react-gtm-module';
 import { AppProps } from 'next/app';
 import { Auth0Provider, useAuth0 } from '@auth0/auth0-react';
-import { config } from '@black-pear-joggers/core-services';
 import { Container } from '@black-pear-joggers/container';
 import { Footer } from '@black-pear-joggers/footer';
 import { Forbidden } from '../components/forbidden';
 import { Header } from '@black-pear-joggers/header';
-import { isAllowedUser, UserWithRoles } from '../helpers/auth';
+import { UserWithRoles } from '../helpers/auth';
 import { LoadingSpinner } from '../components/loading-spinner';
-import { PropsWithChildren, useEffect, useState } from 'react';
+import { PropsWithChildren, useEffect } from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Stack } from '@black-pear-joggers/stack';
-import { useRouter } from 'next/router';
 import './styles.css';
+import { config, tagManagerArgs } from '../helpers/config';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -22,10 +21,6 @@ const queryClient = new QueryClient({
     },
   },
 });
-
-const tagManagerArgs = {
-  gtmId: 'GTM-W8KBB3D',
-};
 
 let getAccessTokenSilently = null;
 
@@ -109,9 +104,7 @@ function CustomApp({ Component, pageProps }: AppProps) {
 
   return (
     <Auth0Provider
-      {...config.auth(
-        typeof window !== 'undefined' && `${window.location.origin}/admin`
-      )}
+      {...config.auth}
     >
       <QueryClientProvider client={queryClient}>
         <div id="modalContainer"></div>
