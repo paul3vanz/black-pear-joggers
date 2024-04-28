605149282ca28bb8d749a947ef4d59611e65f849
Paul Evans <paul3vanz@gmail.com>
2024-04-28 07:24:31 +0100

    add config

---
+++ next/apps/results/helpers/config.ts
@@ -0,0 +1,12 @@
+import { config as sharedConfig } from "@black-pear-joggers/core-services";
+
+export const tagManagerArgs = {
+    gtmId: 'GTM-W8KBB3D',
+};
+
+export const redirectUri = typeof window !== 'undefined' && `${window.location.origin}/profile`;
+
+export const config = {
+    ...sharedConfig,
+    auth: sharedConfig.auth(redirectUri),
+}
