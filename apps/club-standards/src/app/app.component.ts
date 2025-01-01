17fb43f119c584f5e3f79179f36ffa9ee2ca6688
Paul Evans <paul3vanz@gmail.com>
2025-01-01 21:00:16 +0000

    link to new award claim page

--- apps/club-standards/src/app/app.component.ts
+++ apps/club-standards/src/app/app.component.ts
@@ -5,33 +5,32 @@ import { AwardClaim } from './models/award-claim.model';
 import { ClubStandardsService } from './services/club-standards.service';
 
 @Component({
-  selector: 'bpj-root',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.scss' ],
+    selector: 'bpj-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  showCertificate = false;
-  certificate: AwardClaim;
-  title = 'Club standards | BPJ';
+    showCertificate = false;
+    certificate: AwardClaim;
+    title = 'Club standards | BPJ';
 
-  onClaimAwardClick() {
-    // location.href = 'https://docs.google.com/forms/d/e/1FAIpQLSdVYbxf7DYjH0QSWBDmpyV3yyC0RWh9jzrQ-1hoPcyCTGCvzA/viewform';
-    this.store$.dispatch(new fromClubStandardsActions.ClubStandardsClaimStart());
-  }
-
-  constructor(private store$: Store<any>, private clubStandardsService: ClubStandardsService) {}
-
-  ngOnInit() {
-    const searchParams = new URLSearchParams(location.search);
-
-    if (searchParams.get('certificateId')) {
-      const certificateId = searchParams.get('certificateId');
-      const uniqueToken = searchParams.get('token');
-
-      this.clubStandardsService.getAwardClaim(Number(certificateId), uniqueToken).subscribe((certificate) => {
-        this.certificate = certificate;
-        this.showCertificate = true;
-      });
+    onClaimAwardClick() {
+        location.href = 'https://bpj.org.uk/claim-award';
     }
-  }
+
+    constructor(private store$: Store<any>, private clubStandardsService: ClubStandardsService) { }
+
+    ngOnInit() {
+        const searchParams = new URLSearchParams(location.search);
+
+        if (searchParams.get('certificateId')) {
+            const certificateId = searchParams.get('certificateId');
+            const uniqueToken = searchParams.get('token');
+
+            this.clubStandardsService.getAwardClaim(Number(certificateId), uniqueToken).subscribe((certificate) => {
+                this.certificate = certificate;
+                this.showCertificate = true;
+            });
+        }
+    }
 }
