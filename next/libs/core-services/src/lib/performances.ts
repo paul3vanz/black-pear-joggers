69a1c58170afbbae3aa5febc8e34554e15a85767
Paul Evans <paul3vanz@gmail.com>
2025-02-02 21:53:39 +0000

    correctly type optional athlete id

--- next/libs/core-services/src/lib/performances.ts
+++ next/libs/core-services/src/lib/performances.ts
@@ -4,7 +4,7 @@ import { useQuery } from '@tanstack/react-query';
 import { Performance } from './performances/performance.type';
 import { Paginate } from './common/types/paginate.type';
 
-export function usePerformances(athleteId: number) {
+export function usePerformances(athleteId?: number) {
     return useQuery<Paginate<Performance>>(['performances'], () =>
         fetcher(
             `${config.baseApiUrl}/performances?athleteId=${athleteId}&limit=1000`
